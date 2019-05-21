426a38430d58f09d89ef2ab445d5cfd4d2e7c473
MatiasEzr <Lizarraga_matias@hotmail.com.ar>
2019-05-21 04:31:19 -0300

    V0,1
    Subo el codigo de la nave + lo que dejo Gustavo.
    Solo agregue la clase Bandido para ejemplo, se tendría que hacer lo mismo con todas las clases y ademas agregar las listas. Lo importante es el mapa igual.

---
+++ TP AYED 2019 Clash of UNLA/CLASH_OF_UNLA/Minas.cpp
@@ -0,0 +1,90 @@
+#include "Minas.h"
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+void crear_Minas(Minas &minas){
+    minas.posX=0;
+    minas.posY=0;
+    minas.codItem=0;
+    minas.ip=0;
+    minas.seq1=0;
+    minas.seq2=0;
+    minas.seq3=0;
+    minas.seq4=0;
+    minas.seq5=0;
+}
+void destruir_Minas(Minas &minas){}
+
+
+void setPosX(Minas &minas, int pX){
+    minas.posX=pX;
+}
+int getPosX(Minas &minas){
+    return minas.posX;
+}
+
+void setPosY(Minas &minas, int pY){
+    minas.posY=pY;
+}
+
+int getPosY(Minas &minas){
+    return minas.posY;
+}
+
+void setCodItem(Minas &minas, int codI){
+    minas.codItem=codI;
+}
+int getCodItem(Minas &minas){
+    return minas.codItem;
+}
+
+void setIp(Minas &minas, int i){
+    minas.ip=i;
+}
+
+int getIp(Minas &minas){
+    return minas.ip;
+}
+
+void setSeq1(Minas &minas, int s1){
+    minas.seq1=s1;
+}
+
+int getSet1(Minas &minas){
+    return minas.seq1;
+}
+void setSeq2(Minas &minas, int s2){
+    minas.seq2=s2;
+}
+
+int getSet2(Minas &minas){
+    return minas.seq2;
+}
+
+void setSeq3(Minas &minas, int s3){
+    minas.seq3=s3;
+}
+
+int getSet3(Minas &minas){
+    return minas.seq3;
+}
+
+void setSeq4(Minas &minas, int s4){
+    minas.seq4=s4;
+}
+
+int getSet4(Minas &minas){
+    return minas.seq4;
+}
+
+void setSeq5(Minas &minas, int s5){
+    minas.seq5=s5;
+}
+
+int getSeq5(Minas &minas){
+    return minas.seq5;
+}
---
+++ TP AYED 2019 Clash of UNLA/CLASH_OF_UNLA/Terreno.h
@@ -0,0 +1,18 @@
+#ifndef TERRENO_H
+#define TERRENO_H
+
+
+class Terreno
+{
+    public:
+        /** Default constructor */
+        Terreno();
+        /** Default destructor */
+        virtual ~Terreno();
+
+    protected:
+
+    private:
+};
+
+#endif // TERRENO_H
